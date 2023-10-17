d8bd2f1fe6a26c1330496d9135c29db66a416c67
Alex Deliberali <37247632+AlexDeliberali@users.noreply.github.com>
2023-10-17 18:52:03 -0300

    Commit 17/10/23
    Adicionado métodos Adicionar/ExibirLista/Remover conta

--- Bytebank_Atendimento/Program.cs
+++ Bytebank_Atendimento/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using bytebank.Modelos.Conta;
+using Bytebank_Atendimento.bytebank.Util;
+
+void TestaArrayDeContasCorrentes()
+{
+    ListaDeContaCorrente listaDeContas = new ListaDeContaCorrente();
+
+    listaDeContas.Adicionar(new ContaCorrente(874, "5679787-A"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "4456668-B"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-C"));
+
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-D"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-E"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-F"));
+
+    var contaDoAndre = new ContaCorrente(963, "123456-X");
+    listaDeContas.Adicionar(contaDoAndre);
+    listaDeContas.ExibeLista();
+    Console.WriteLine("Agora irá remover a conta!");
+    listaDeContas.Remover(contaDoAndre);
+    listaDeContas.ExibeLista();
+    Console.WriteLine("Conta removida!");
+}
+
+TestaArrayDeContasCorrentes();
